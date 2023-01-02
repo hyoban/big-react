b3e3a2153b17ff256815e4d781f0a0316bb4b8ca
hyoban <hi@hyoban.cc>
2023-01-02 11:59:23 +0800

    chore: review 2-2

--- scripts/rollup/react.config.js
+++ scripts/rollup/react.config.js
@@ -8,12 +8,16 @@ const pkgPath = resolvePkgPath(name)
 const pkgDistPath = resolvePkgPath(name, true)
 
 export default [
-  // react
+  // React
   {
     input: `${pkgPath}/${module}`,
     output: {
       file: `${pkgDistPath}/index.js`,
+      // output 中 name 字段的值应为 react 而不是 index.js
+      // 对于 umd 的格式，如果是以 esm 引入，则两者没有区别。
+      // 但如果在浏览器引入，前者会挂载 window.react，而后者会挂载 window.index。
       name: 'React',
+      // umd: 兼容 commonjs、esm
       format: 'umd',
     },
     plugins: [
@@ -21,6 +25,7 @@ export default [
       generatePackageJson({
         inputFolder: pkgPath,
         outputFolder: pkgDistPath,
+        // 不直接将 package.json 中的全部内容写入
         baseContents: ({ name, description, version }) => ({
           name,
           description,
--- scripts/rollup/utils.js
+++ scripts/rollup/utils.js
@@ -1,13 +1,21 @@
 import path from 'path'
 import fs from 'fs'
 
-import ts from 'rollup-plugin-typescript2'
+// rollup 原生支持 ESM 格式，所以对于 CJS 格式的包，我们需要先将它用该插件转为 ESM 格式。
+// 这里使用该插件是因为他是 rollup 中最常见的插件，加上有备无患。
 import cjs from '@rollup/plugin-commonjs'
+import ts from 'rollup-plugin-typescript2'
 import replace from '@rollup/plugin-replace'
 
 const pkgPath = path.resolve(__dirname, '../../packages')
+// 产物路径，打包的产物会有多个
 const distPath = path.resolve(__dirname, '../../dist/node_modules')
 
+/**
+ * @param {string} pkgName 包名
+ * @param {boolean} isDist 是否为产物路径
+ * @returns 包所处的路径
+ */
 export function resolvePkgPath(pkgName, isDist) {
   if (isDist) {
     return `${distPath}/${pkgName}`
@@ -16,6 +24,10 @@ export function resolvePkgPath(pkgName, isDist) {
   }
 }
 
+/**
+ * @param {string} pkgName 包名
+ * @returns 解析包的package.json
+ */
 export function getPackageJSON(pkgName) {
   const path = `${resolvePkgPath(pkgName)}/package.json`
   const str = fs.readFileSync(path, 'utf8')
