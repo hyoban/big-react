85e24aa05b7ab59cf7a73618fd5379b3f9eb3e1b
hyoban <hi@hyoban.cc>
2023-01-02 17:08:11 +0800

    chore: review 6-2

--- packages/react-reconciler/src/commitWork.ts
+++ packages/react-reconciler/src/commitWork.ts
@@ -6,9 +6,9 @@ import { FunctionComponent, HostComponent, HostRoot, HostText } from './workTags
 
 let nextEffect: FiberNode | null = null
 
-export const commitMutationEffects = (
+export function commitMutationEffects(
   finishedWork: FiberNode,
-) => {
+) {
   nextEffect = finishedWork
 
   while (nextEffect !== null) {
@@ -17,6 +17,7 @@ export const commitMutationEffects = (
     if ((nextEffect.subtreeFlags & MutationMask) !== NoFlags && child !== null) {
       nextEffect = child
     } else {
+      // 要么没有子节点，要么子节点没有 flags
       // 向上遍历
       while (nextEffect !== null) {
         commitMutationEffectsOnFiber(nextEffect)
@@ -32,10 +33,12 @@ export const commitMutationEffects = (
 }
 
 function commitMutationEffectsOnFiber(finishedWork: FiberNode) {
+  // 此处的 finishedWork 就是真正需要处理 flags 的节点
   const flags = finishedWork.flags
 
   if ((flags & Placement) !== NoFlags) {
     commitPlacement(finishedWork)
+    // 将 Placement 标记从 flags 中移除
     finishedWork.flags &= ~Placement
   }
 
@@ -123,18 +126,27 @@ function commitNestedComponent(
 
 function commitPlacement(finishedWork: FiberNode) {
   if (__DEV__) {
-    console.warn('commitPlacement', finishedWork)
+    console.warn('(commitPlacement)', '执行 Placement 操作', finishedWork)
   }
+
+  // 找到最近的父级节点
   const hostParent = getHostParent(finishedWork)
   if (hostParent !== null) {
+    // 将 finishedWork 对应的 DOM 节点插入到父级节点中
     appendPlacementNodeIntoContainer(finishedWork, hostParent)
   }
 }
 
-function getHostParent(fiber: FiberNode) {
+/**
+ * 向上找到 fiber 最近的包含实际宿主环境节点 fiber 的宿主节点
+ * @param fiber
+ * @returns
+ */
+function getHostParent(fiber: FiberNode): Container | null {
   let parent = fiber.return
   while (parent !== null) {
     const parentTag = parent.tag
+
     if (parentTag === HostRoot) {
       return (parent.stateNode as FiberRootNode).container
     }
@@ -145,11 +157,18 @@ function getHostParent(fiber: FiberNode) {
     parent = parent.return
   }
   if (__DEV__) {
-    console.warn('getHostParent not found', fiber)
+    console.warn('(getHostParent)', '未找到宿主节点', fiber)
   }
   return null
 }
 
+/**
+ * 将标记 Placement 的 fiber 节点对应的宿主环境节点插入到父宿主节点中。
+ * 传入的 fiber 不一定有对应宿主环境节点，需要向下递归找到实际的 host node。
+ * @param finishedWork
+ * @param hostParent
+ * @returns
+ */
 function appendPlacementNodeIntoContainer(
   finishedWork: FiberNode,
   hostParent: Container,
