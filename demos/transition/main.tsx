20b4924d2da0efba73e50a251b94f48b1d501802
Stephen Zhou <hi@hyoban.cc>
2023-12-14 17:29:58 +0800

    feat: 「19-1」useTransition的作用

---
+++ demos/transition/main.tsx
@@ -0,0 +1,46 @@
+// import { useState, useTransition } from 'react';
+import { useState } from "react"
+import ReactDOM from "react-dom"
+
+import AboutTab from "./AboutTab"
+import ContactTab from "./ContactTab"
+import PostsTab from "./PostsTab"
+import TabButton from "./TabButton"
+
+import "./style.css"
+
+function App() {
+  // const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState("about")
+  console.log("hello")
+  function selectTab(nextTab) {
+    // startTransition(() => {
+    setTab(nextTab)
+    // });
+  }
+
+  return (
+    <>
+      <TabButton isActive={tab === "about"} onClick={() => selectTab("about")}>
+        首页
+      </TabButton>
+      <TabButton isActive={tab === "posts"} onClick={() => selectTab("posts")}>
+        博客 (render慢)
+      </TabButton>
+      <TabButton
+        isActive={tab === "contact"}
+        onClick={() => selectTab("contact")}
+      >
+        联系我
+      </TabButton>
+      <hr />
+      {tab === "about" && <AboutTab />}
+      {tab === "posts" && <PostsTab />}
+      {tab === "contact" && <ContactTab />}
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+root.render(<App />)
--- packages/react-reconciler/src/fiberReconciler.ts
+++ packages/react-reconciler/src/fiberReconciler.ts
@@ -1,3 +1,5 @@
+import { unstable_ImmediatePriority, unstable_runWithPriority } from "scheduler"
+
 import { FiberNode, FiberRootNode } from "./fiber"
 import { requestUpdateLane } from "./fiberLanes"
 import { createUpdate, createUpdateQueue, enqueueUpdate } from "./updateQueue"
@@ -35,18 +37,21 @@ export function updateContainer(
   element: ReactElementType | null,
   root: FiberRootNode,
 ) {
-  const hostRootFiber = root.current
+  // 默认同步更新
+  unstable_runWithPriority(unstable_ImmediatePriority, () => {
+    const hostRootFiber = root.current
 
-  // 首屏渲染，触发更新，在 beginWork 和 completeWork 中处理更新
-  const lane = requestUpdateLane()
-  const update = createUpdate<ReactElementType | null>(element, lane)
-  enqueueUpdate(
-    hostRootFiber.updateQueue as UpdateQueue<ReactElementType | null>,
-    update,
-  )
+    // 首屏渲染，触发更新，在 beginWork 和 completeWork 中处理更新
+    const lane = requestUpdateLane()
+    const update = createUpdate<ReactElementType | null>(element, lane)
+    enqueueUpdate(
+      hostRootFiber.updateQueue as UpdateQueue<ReactElementType | null>,
+      update,
+    )
 
-  // 调度更新，连接 container 和 renderRoot 的更新流程
-  scheduleUpdateOnFiber(hostRootFiber, lane)
+    // 调度更新，连接 container 和 renderRoot 的更新流程
+    scheduleUpdateOnFiber(hostRootFiber, lane)
+  })
 
   return element
 }
